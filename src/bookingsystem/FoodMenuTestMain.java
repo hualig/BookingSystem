af9bffbaddee72806bc4f6801665ce9b901df99f
User <User@8LZ7J32>
2016-11-16 15:20:52 +0100

    Merge origin/master
    Conflicts:
    	nbproject/genfiles.properties

--- src/bookingsystem/FoodMenuTestMain.java
+++ src/bookingsystem/FoodMenuTestMain.java
@@ -10,19 +10,34 @@ package bookingsystem;
  * @author User
  */
 public class FoodMenuTestMain {
+    //Leave this uncommented. Used for both tests
     static Customer customer1 = new Customer();
-    static Stub_CustomerMenuPresentation run = new Stub_CustomerMenuPresentation();
+    
+    //Uncomment to test findByName method in FoodMenu
+    //
+//    static FoodMenu foodmenu = new FoodMenu();
+//    static Food food1 = new Food("Lasagne", 79.66);
+
     
     
-    public FoodMenuTestMain(){
+    //Uncomment to test Stub_CustomerMenuPresentation
+    //
+//    static Stub_CustomerMenuPresentation run = new Stub_CustomerMenuPresentation();
+//    FoodInit runFoodInit = new FoodInit();
+
+    public static void main(String[] args) {
         
+      //Uncomment to test findByName method in FoodMenu
+      //
+//        foodmenu.addFoodItem(food1);
+//        Food foundFood = foodmenu.findFoodByName("Lasagne");
+//        System.out.println(foundFood);
+
+
+        
+        //Uncomment to test Stub_CustomerMenuPresentation
+        //
+//        run.doStuff();
     }
-    
-    
-    
-    
-    public static void main (String[] args){
-        run.doStuff();
-    }
-    
+
 }
