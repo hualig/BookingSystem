315408e9bec4426abdd3fee0ab46ab0d249ef8bf
Mikael Tolfström <User@1HZ7J32>
2016-11-16 15:07:59 +0100

    by Tolfström

--- src/bookingsystem/FoodMenuTestMain.java
+++ src/bookingsystem/FoodMenuTestMain.java
@@ -10,22 +10,34 @@ package bookingsystem;
  * @author User
  */
 public class FoodMenuTestMain {
-
+    //Leave this uncommented. Used for both tests
     static Customer customer1 = new Customer();
     
-    
+    //Uncomment to test findByName method in FoodMenu
+    //
 //    static FoodMenu foodmenu = new FoodMenu();
-//    static Food food1 = new Food("Lasagne", 79.99);
+//    static Food food1 = new Food("Lasagne", 79.66);
 
-    static Stub_CustomerMenuPresentation run = new Stub_CustomerMenuPresentation();
-    FoodInit runFoodInit = new FoodInit();
+    
+    
+    //Uncomment to test Stub_CustomerMenuPresentation
+    //
+//    static Stub_CustomerMenuPresentation run = new Stub_CustomerMenuPresentation();
+//    FoodInit runFoodInit = new FoodInit();
 
     public static void main(String[] args) {
+        
+      //Uncomment to test findByName method in FoodMenu
+      //
 //        foodmenu.addFoodItem(food1);
 //        Food foundFood = foodmenu.findFoodByName("Lasagne");
 //        System.out.println(foundFood);
 
-        run.doStuff();
+
+        
+        //Uncomment to test Stub_CustomerMenuPresentation
+        //
+//        run.doStuff();
     }
 
 }
